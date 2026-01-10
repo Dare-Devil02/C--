6c5d718a0971a9e2e69b46ac5cfe5c4e4d47b216
Dare-Devil <ap.devil48@gmail.com>
2026-01-10 13:35:35 +0530

    C plus plus starts

---
+++ datatypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=15;
+    char b='d';
+    float e=1.2;
+    double f=15.764;
+    cout<<"Value of integer is "<<a<<endl;
+    cout<<"typecasting"<<endl;
+    cout<<"value of b in integer is "<<(int)b<<endl;
+}
