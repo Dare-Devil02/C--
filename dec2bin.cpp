37295ab80169db2a346a2b71673358fc1808abf2
Dare-Devil <ap.devil48@gmail.com>
2026-01-17 12:28:28 +0530

    new programs

---
+++ dec2bin.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n,i=0;
+    cout<<"Enter a number :";
+    cin>>n;
+    int ans=0;
+    while(n!=0){
+        int bit=n & 1;
+        ans=(bit*pow(10,i))+ans;
+        n=n>>1;
+        i++;
+    }
+    cout<<"Answer :"<<ans<<endl;
+}
